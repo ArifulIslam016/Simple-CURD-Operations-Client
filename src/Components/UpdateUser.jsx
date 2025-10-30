b33e37d9d8cbb46d5b79bdc67e90b961c2d3c194
Ariful Islam <arifulq234@gmail.com>
2025-10-30 17:15:59 +0600

    Update Functionality added

--- src/Components/UpdateUser.jsx
+++ src/Components/UpdateUser.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
+import React from "react";
+import { useLoaderData } from "react-router";
 
 const UpdateUser = () => {
-    const user=useLoaderData()
-    const hanldeUpdateUser=(e)=>{
-        e.preventDefault()
-        const name=e.target.name.value;
-        const email=e.target.email.value;
-        console.log(name,email)
-    }
-    return (
-        <div>
-            <h1>Update user here</h1>
-            <form onSubmit={hanldeUpdateUser}>
-                <input type="text" name='name'  defaultValue={user.name} /><br />
-                <input type="email" name="email" id="" defaultValue={user.email} /><br />
-                <input type="submit" value="Edit User" />
-
-            </form>
-        </div>
-    );
+  const user = useLoaderData();
+  const hanldeUpdateUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+    fetch(`http://localhost:3000/users/${user._id}`,{
+        method:'PATCH',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify({name,email})
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  return (
+    <div>
+      <h1>Update user here</h1>
+      <form onSubmit={hanldeUpdateUser}>
+        <input type="text" name="name" defaultValue={user.name} />
+        <br />
+        <input type="email" name="email" id="" defaultValue={user.email} />
+        <br />
+        <input type="submit" value="Edit User" />
+      </form>
+    </div>
+  );
 };
 
 export default UpdateUser;
